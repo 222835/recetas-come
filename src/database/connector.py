cfa41b06919be9f1d9e092d09980ed56d7dcd202
Fernando <ponceconc@outlook.com>
2025-03-15 01:49:38 -0600

    database connector ended

--- src/database/connector.py
+++ src/database/connector.py
@@ -1,19 +1,23 @@
-from typing import Self
 import mariadb as db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine.base import Connection
 
+Base = declarative_base()
 
 ## @brief This class is responsible for connecting to the database and executing queries
 class Connector:
     ## @brief Connects to the database
-    def __init__(self):
-        self.connection = db.connect("localhost",
-                                     user="root",
-                                     password="password",
-                                     database="test")
+    def __init__(self, db_url="mysql+mariadb://root:password@localhost/test"):  # Updated connection string
+        self.engine = create_engine(db_url)
+        Base.metadata.create_all(self.engine)  # Create tables
+        self.Session = sessionmaker(bind=self.engine)
+        self.connection = self.engine.connect() # Establish connection
 
     ## @brief Returns the connection object
     ## @return Self The connection object
-    def get_connection(self) -> Self:
+    def get_connection(self) -> Connection: # Removed Self return type
         return self.connection
 
     ## @brief Closes the connection
@@ -23,14 +27,10 @@ class Connector:
     ## @brief Executes the query and returns the result
     ## @param query The query to be executed
     ## @return The result of the query
-    def execute_query(self, query:str):
-        cursor = self.connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+    def execute_query(self, query): # Removed type annotation
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
 
-
-if __name__ == "__main__":
-    input("This file is not meant to be run directly. Press enter to exit.")
-    connector = Connector()
-    print(connector.execute_query("SELECT * FROM users"))
-    connector.close_connection()
+    def get_session(self):
+        return self.Session()
