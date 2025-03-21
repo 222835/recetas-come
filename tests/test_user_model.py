881098ad5c420f0c9a738081d4e96f9311dc574b
al223053@alumnos.uacj.mx <al223053@alumnos.uacj.mx>
2025-03-20 20:09:18 -0600

    Correccion de error

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -24,6 +24,7 @@ class TestUsuarioModel(unittest.TestCase):
         self.session.close()
         Base.metadata.drop_all(self.engine)
         self.engine.dispose()
+
     def test_create_usuario(self):
         """Test creating a new Usuario."""
         hashed_password = "password123"
@@ -35,7 +36,6 @@ class TestUsuarioModel(unittest.TestCase):
         self.assertIsNotNone(new_user.numero_usuario)
         self.assertEqual(new_user.nombre_completo, "Test User")
         self.assertEqual(new_user.nombre_usuario, "testuser")
-        #self.assertEqual(new_user.contrasenia, Security.generate_password(hashed_password))
         self.assertTrue(Security.verify_password(hashed_password, new_user.contrasenia))
         self.assertEqual(new_user.rol, "user")
 
@@ -56,23 +56,6 @@ class TestUsuarioModel(unittest.TestCase):
         self.assertEqual(retrieved_user.rol, "user")
         self.assertTrue(Security.verify_password(hashed_password, retrieved_user.contrasenia))
 
-    def test_read_usuario_by_username(self):
-        """Test reading a Usuario from the database by username."""
-        hashed_password = "password123"
-        new_user = Usuario(nombre_completo="Test User", contrasenia=hashed_password, rol="user", nombre_usuario="testuser")
-        self.session.add(new_user)
-        self.session.commit()
-
-        # Read the user from the database by username
-        retrieved_user = self.session.query(Usuario).filter_by(nombre_usuario="testuser").first()
-
-        # Assert that the retrieved user is the same as the created user
-        self.assertEqual(retrieved_user.numero_usuario, new_user.numero_usuario)
-        self.assertEqual(retrieved_user.nombre_completo, "Test User")
-        self.assertTrue(Security.verify_password(hashed_password, retrieved_user.contrasenia))
-        self.assertEqual(retrieved_user.contrasenia, Security.generate_password(hashed_password))
-        self.assertEqual(retrieved_user.rol, "user")
-
     def test_update_usuario(self):
         """Test updating a Usuario in the database."""
         hashed_password = "password123"
@@ -84,8 +67,7 @@ class TestUsuarioModel(unittest.TestCase):
         new_hashed_password = "new_password"
         new_user.nombre_completo = "New Test User"
         new_user.nombre_usuario = "newtestuser"
-        new_user.update(self.session, contrasenia=new_hashed_password) # Proposed change for lower commented line
-        #new_user.contrasenia = new_hashed_password
+        new_user.update(self.session, contrasenia=new_hashed_password)
         new_user.rol = "admin"
         self.session.commit()
 
@@ -95,7 +77,7 @@ class TestUsuarioModel(unittest.TestCase):
         # Assert that the user's information was updated correctly
         self.assertEqual(updated_user.nombre_completo, "New Test User")
         self.assertEqual(updated_user.nombre_usuario, "newtestuser")
-        self.assertTrue(Security.verify_password(new_hashed_password, updated_user.contrasenia)) # Updated assertion
+        self.assertTrue(Security.verify_password(new_hashed_password, updated_user.contrasenia))
         self.assertEqual(updated_user.rol, "admin")
 
     def test_delete_usuario(self):
@@ -114,66 +96,66 @@ class TestUsuarioModel(unittest.TestCase):
         self.assertIsNone(deleted_user)
     
     def test_authenticate(self):
-            """Test the authentication of a user."""
-            hashed_password = "password123"
-            new_user = Usuario(nombre_completo="Test User", contrasenia=hashed_password, rol="user", nombre_usuario="testuser")
-            self.session.add(new_user)
-            self.session.commit()
+        """Test the authentication of a user."""
+        hashed_password = "password123"
+        new_user = Usuario(nombre_completo="Test User", contrasenia=hashed_password, rol="user", nombre_usuario="testuser")
+        self.session.add(new_user)
+        self.session.commit()
 
-            # Try to authenticate with the correct credentials
-            authenticated_user = AuthService.authenticate(self.session, "testuser", hashed_password)
+        # Try to authenticate with the correct credentials
+        authenticated_user = AuthService.authenticate(self.session, "testuser", hashed_password)
 
-            # Assert that the authentication was successful and the correct user was returned
-            self.assertIsNotNone(authenticated_user)
-            self.assertEqual(authenticated_user.nombre_usuario, "testuser")
-            self.assertTrue(Security.verify_password(hashed_password, authenticated_user.contrasenia))
+        # Assert that the authentication was successful and the correct user was returned
+        self.assertIsNotNone(authenticated_user)
+        self.assertEqual(authenticated_user.nombre_usuario, "testuser")
+        self.assertTrue(Security.verify_password(hashed_password, authenticated_user.contrasenia))
 
-            # Try to authenticate with the incorrect username
-            authenticated_user_invalid_username = AuthService.authenticate(self.session, "invalidtestuser", hashed_password)
+        # Try to authenticate with the incorrect username
+        authenticated_user_invalid_username = AuthService.authenticate(self.session, "invalidtestuser", hashed_password)
 
-            # Assert that authentication failed with incorrect username
-            self.assertIsNone(authenticated_user_invalid_username)
+        # Assert that authentication failed with incorrect username
+        self.assertIsNone(authenticated_user_invalid_username)
 
-            # Try to authenticate with incorrect credentials
-            authenticated_user_invalid_password = AuthService.authenticate(self.session, "testuser", "wrongpassword")
+        # Try to authenticate with incorrect credentials
+        authenticated_user_invalid_password = AuthService.authenticate(self.session, "testuser", "wrongpassword")
 
-            # Assert that authentication failed with incorrect password
-            self.assertIsNone(authenticated_user_invalid_password)
+        # Assert that authentication failed with incorrect password
+        self.assertIsNone(authenticated_user_invalid_password)
     
     def test_edit_account_info_admin(self):
-            """Tests that an admin can edit other users information."""
-            new_user_admin = Usuario(nombre_completo="Nombre Admin", contrasenia="admin123", rol="admin", nombre_usuario="adminuser")
-            new_user_invitado = Usuario(nombre_completo="Test User", contrasenia="password123", rol="invitado", nombre_usuario="testuser")
-            self.session.add_all([new_user_admin, new_user_invitado])
-            self.session.commit()
+        """Tests that an admin can edit other users information."""
+        new_user_admin = Usuario(nombre_completo="Nombre Admin", contrasenia="admin123", rol="admin", nombre_usuario="adminuser")
+        new_user_invitado = Usuario(nombre_completo="Test User", contrasenia="password123", rol="invitado", nombre_usuario="testuser")
+        self.session.add_all([new_user_admin, new_user_invitado])
+        self.session.commit()
 
-            # Admin modifies another users information
-            new_hashed_password = "newpassword"
-            Usuario.edit_account_info(new_user_admin, new_user_invitado, self.session, nombre_completo="Updated User", nombre_usuario="updateduser",
-                                    contrasenia=new_hashed_password)
+        # Admin modifies another users information
+        new_hashed_password = "newpassword"
+        Usuario.edit_account_info(new_user_admin, new_user_invitado, self.session, nombre_completo="Updated User", nombre_usuario="updateduser", contrasenia=new_hashed_password)
 
-            updated_user = self.session.query(Usuario).filter_by(numero_usuario=new_user_invitado.numero_usuario).first()
+        updated_user = self.session.query(Usuario).filter_by(numero_usuario=new_user_invitado.numero_usuario).first()
 
-            # Asserts that changes have been made
-            self.assertEqual(updated_user.nombre_completo, "Updated User")
-            self.assertEqual(updated_user.nombre_usuario, "updateduser")
-            self.assertTrue(Security.verify_password(new_hashed_password, updated_user.contrasenia))
+        # Asserts that changes have been made
+        self.assertEqual(updated_user.nombre_completo, "Updated User")
+        self.assertEqual(updated_user.nombre_usuario, "updateduser")
+        self.assertTrue(Security.verify_password(new_hashed_password, updated_user.contrasenia))
+
     def test_edit_account_info_self(self):
-            """Tests that users can edit their own username and password."""
-            hashed_password = "password123"
-            new_user = Usuario(nombre_completo="Test User", contrasenia= hashed_password, rol="invitado", nombre_usuario="testuser")
-            self.session.add(new_user)
-            self.session.commit()
+        """Tests that users can edit their own username and password."""
+        hashed_password = "password123"
+        new_user = Usuario(nombre_completo="Test User", contrasenia=hashed_password, rol="invitado", nombre_usuario="testuser")
+        self.session.add(new_user)
+        self.session.commit()
 
-            # User edits their own information
-            new_hashed_password = "newpassword"
-            Usuario.edit_account_info(new_user, new_user, self.session, nombre_usuario="newusername", contrasenia=new_hashed_password)
+        # User edits their own information
+        new_hashed_password = "newpassword"
+        Usuario.edit_account_info(new_user, new_user, self.session, nombre_usuario="newusername", contrasenia=new_hashed_password)
 
-            updated_user = self.session.query(Usuario).filter_by(numero_usuario=new_user.numero_usuario).first()
+        updated_user = self.session.query(Usuario).filter_by(numero_usuario=new_user.numero_usuario).first()
 
-            # Asserts that changes have been made
-            self.assertEqual(updated_user.nombre_usuario, "newusername")
-            self.assertTrue(Security.verify_password(new_hashed_password, updated_user.contrasenia))
+        # Asserts that changes have been made
+        self.assertEqual(updated_user.nombre_usuario, "newusername")
+        self.assertTrue(Security.verify_password(new_hashed_password, updated_user.contrasenia))
 
     def test_edit_account_info_no_permission(self):
         """Tests that a guest user cannot edit another users info."""
