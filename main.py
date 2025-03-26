13bd6d7f32c6843765ca9b0972828584e0297d5e
Chinano9 <ponceconc@outlook.com>
2025-03-25 21:29:05 -0600

    MySQL error arreglado

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.utils.constants import env as env  # Import constants
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))  # Define ROOT_PATH here
 constants.init(ROOT_PATH) # Initialize constants
 
-from src.Users.Login.view import LoginApp
+#from src.Users.Login.view import LoginApp
 
 
 ## @brief Main function
@@ -14,10 +14,10 @@ from src.Users.Login.view import LoginApp
 if __name__ == '__main__':
     print("database initialized")
     print(f"mysql+pymysql://{env["DB_USER"]}:{env["DB_PASSWORD"]}@{env["DB_HOST"]}:3307/{env["DB_DATABASE"]}")
-    connector = Connector(f"mariadb://{env["DB_USER"]}:{env["DB_PASSWORD"]}@{env["DB_HOST"]}:3307/{env["DB_DATABASE"]}")
+    connector = Connector(f"mysql+pymysql://{env["DB_USER"]}:{env["DB_PASSWORD"]}@{env["DB_HOST"]}:3306/{env["DB_DATABASE"]}")
 
-    login_view = LoginApp()
-    login_view.mainloop()
+    #login_view = LoginApp()
+    #login_view.mainloop()
 
     #connector.close_connection()
     print("database closed")
