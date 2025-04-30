b3f8e0737f4d23837db5f9674cdd39b54d5bf0b3
Fernando <ponceconc@outlook.com>
2025-04-30 14:08:24 -0600

    Modificar las tablas Usuarios y Proveedores para que los campos de ID sean AUTO_INCREMENT

--- src/database/db_init.sql
+++ src/database/db_init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE COME;
 USE COME;
 
 CREATE TABLE Usuarios (
-    numero_usuario INT PRIMARY KEY,
+    numero_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre_completo VARCHAR(100) NOT NULL,
     nombre_usuario VARCHAR(50) NOT NULL,
     contrasenia VARCHAR(50),
@@ -11,7 +11,7 @@ CREATE TABLE Usuarios (
 );
 
 CREATE TABLE Proveedores (
-    id_proveedor INT PRIMARY KEY,
+    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     categoria VARCHAR(50)
 );
