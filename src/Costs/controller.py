89105f9831540f4810dc6a059b73a8f50346d1e2
Chinano9 <ponceconc@outlook.com>
2025-05-07 01:45:18 -0600

    Agregar métodos para eliminar costos, crear múltiples costos y obtener costos por proveedor y nombre en la clase CostController. También se añadió la funcionalidad para importar costos desde un archivo Excel.

--- src/Costs/controller.py
+++ src/Costs/controller.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pandas as pd
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
@@ -79,6 +80,12 @@ class CostController:
         cost.update(session)  # Use the model's update method
         return cost
 
+    ## @brief Delete a cost from the database.
+    ## @param session: The database session.
+    ## @param id_cost: The ID of the cost to delete.
+    ## @return: True if the cost was deleted successfully, False otherwise.
+    ## @details This method deletes a cost from the database based on its ID.
+    ## @note: This method uses the delete method of the Costos model to remove the cost.
     @staticmethod
     def delete_cost(session: Session, id_cost: int) -> bool:
         """Delete a cost from the database."""
@@ -89,27 +96,56 @@ class CostController:
         cost.delete(session)  # Use the model's delete method
         return True
     
+    ## @brief Create multiple costs in the database.
+    ## @param session: The database session.
+    ## @param costs: A list of Costos objects to insert.
+    ## @return: None
+    ## @details This method inserts multiple costs into the database.
+    ## @note: This method uses the bulk_create method of the Costos model to insert multiple costs.
     @staticmethod
     def create_costs(session: Session, costs: list[Costos]) -> None:
         """Insert multiple costs into the database."""
         Costos().bulk_create(session, costs)
 
+    ## @brief Fetch all costs from the database.
+    ## @param session: The database session.
+    ## @return: A list of Costos objects.
+    ## @details This method retrieves all costs from the database.
+    ## @note: This method uses the get_all_costs method of the Costos model to fetch all costs.
     @staticmethod
     def get_all_costs(session: Session) -> list[Costos]:
         """Fetch all costs from the database."""
         cost = Costos()
         return cost.get_all_costs(session)  # Use the model's get_all_costs method
     
+    ## @brief Fetch all costs by provider from the database.
+    ## @param session: The database session.
+    ## @param id_proveedor: The ID of the provider.
+    ## @return: A list of Costos objects that belong to the provider.
+    ## @details This method retrieves costs from the database based on the provided provider ID.
+    ## @note: This method uses the filter method to fetch costs by provider ID.
     @staticmethod
     def fetch_costs_by_provider(session: Session, id_proveedor: int) -> list[Costos]:
         """Fetch all costs for a specific provider."""
         return session.query(Costos).filter(Costos.id_proveedor == id_proveedor).all()
     
+    ## @brief Fetch all costs by name from the database.
+    ## @param session: The database session.
+    ## @param nombre: The name of the cost to fetch.
+    ## @return: A list of Costos objects that match the name.
+    ## @details This method retrieves costs from the database based on the provided name.
+    ## @note: This method uses the filter method to fetch costs by name.
     @staticmethod
     def fetch_costs_by_name(session: Session, nombre: str) -> list[Costos]:
         """Fetch all costs by name."""
         return session.query(Costos).filter(Costos.nombre == nombre).all()
     
+    ## @brief Search for costs by name in the database.
+    ## @param session: The database session.
+    ## @param nombre: The name of the cost to search for.
+    ## @return: A list of Costos objects that match the search criteria.
+    ## @details This method retrieves costs from the database based on the provided name.
+    ## @note: This method uses the ilike method for case-insensitive search.
     @staticmethod
     def search_costs(session: Session, nombre: str) -> list[Costos]:
         """Search for costs by name."""
@@ -131,4 +167,19 @@ class CostController:
         return sorted_costs
     
     
-    
+    @staticmethod
+    def add_costs_for_provider_from_excel(session: Session, id_proveedor: int, file_location:str) -> list[Costos]:
+        """Add costs for a provider from an Excel file."""
+        
+        # Read the Excel file
+        df = pd.read_excel(file_location)
+        
+        costos = [Costos(nombre=row['nombre'], precio=row['precio'], id_proveedor=id_proveedor) for index, row in df.iterrows()]
+        
+        # Insert the costs into the database
+        CostController.create_costs(session, costos)
+
+        
+        
+        return costos
+        
